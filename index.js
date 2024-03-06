033554ef451aacf436bf0b89e53889b2489c30dd
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-06 18:53:32 +0500

    added post text in index.js and refactoring

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-let post = '';
-const titleInputNode = document.querySelector('[data-find="postTitleInput"]');
+let post = {
+    title: '',
+    text: ''
+};
+const postTitleInputNode = document.querySelector('[data-find="postTitleInput"]');
+const postTextInputNode = document.querySelector('[data-find="postTextInput"]');
 const newPostButtonNode = document.getElementById('newPostButton');
 const postsNode = document.querySelector('[data-find="posts"]')
 
@@ -12,15 +16,32 @@ newPostButtonNode.addEventListener('click', function () {
 });
 
 function getPostFromUser() {
-    const post = titleInputNode.value;
-    
-    return post;
+    const title = postTitleInputNode.value;
+    const text = postTextInputNode.value;
+
+    return {
+        title: title,
+        text: text
+    };
 }
 
 function setPost(newPost) {
     post = newPost;
 }
 
+function getPost() {
+    return post;
+}
+
 function renderPost() {
-    postsNode.innerText = post;
+    const post = getPost();
+
+   const postHTML = `
+        <div class='post'>
+            <p class='post__title'>${post.title}</p>
+            <p class='post__text'>${post.text}</p>
+        </div>
+   `;
+
+   postsNode.innerHTML = postHTML;
 }
