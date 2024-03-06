1d2222a479f9ed7a6f12aa5a3502d41e6b203254
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-06 15:38:09 +0500

    posts section added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(1);
+let postTitle = '';
+const titleInputNode = document.querySelector('[data-find="titleInput"]');
+const newPostButtonNode = document.getElementById('newPostButton');
+
+newPostButtonNode.addEventListener('click', function () {
+    postTitle = titleInputNode.value;
+
+    console.log(postTitle);
+});
