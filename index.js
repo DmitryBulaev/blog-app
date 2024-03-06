9c1b7b07e55c80d5dc48e1a5d83f02f709122708
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-06 15:53:35 +0500

    added the ability to display the post title

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let postTitle = '';
 const titleInputNode = document.querySelector('[data-find="titleInput"]');
 const newPostButtonNode = document.getElementById('newPostButton');
+const postsNode = document.querySelector('[data-find="posts"]')
 
 newPostButtonNode.addEventListener('click', function () {
     postTitle = titleInputNode.value;
 
-    console.log(postTitle);
+    postsNode.innerText = postTitle;
 });
+
