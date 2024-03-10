ba1320fa19c8f1222b3f47e2fd71d1ff4d619298
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-10 14:48:37 +0500

    code formatting

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 const posts = [];
-const postTitleInputNode = document.querySelector('[data-find="postTitleInput"]');
+const postTitleInputNode = document.querySelector(
+  '[data-find="postTitleInput"]'
+);
 const postTextInputNode = document.querySelector('[data-find="postTextInput"]');
-const newPostButtonNode = document.getElementById('newPostButton');
-const postsNode = document.querySelector('[data-find="posts"]')
+const newPostButtonNode = document.getElementById("newPostButton");
+const postsNode = document.querySelector('[data-find="posts"]');
 
-newPostButtonNode.addEventListener('click', function () {
-    const postFromUser = getPostFromUser();
-    
-    addPost(postFromUser);
+newPostButtonNode.addEventListener("click", function () {
+  const postFromUser = getPostFromUser();
 
-    renderPosts();
+  addPost(postFromUser);
+
+  renderPosts();
 });
 
 function getPostFromUser() {
-    const title = postTitleInputNode.value;
-    const text = postTextInputNode.value;
+  const title = postTitleInputNode.value;
+  const text = postTextInputNode.value;
 
-    return {
-        title: title,
-        text: text
-    };
+  return {
+    title: title,
+    text: text,
+  };
 }
 
-function addPost({title, text}) {
-    posts.push({
-        title,
-        text
-    });
+function addPost({ title, text }) {
+  posts.push({
+    title,
+    text,
+  });
 }
 
 function getPosts() {
-    return posts;
+  return posts;
 }
 
 let date = new Date();
 
 options = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric"
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
 };
 
-const POSTS_DATE = new Intl.DateTimeFormat('ru', options).format(date);
+const POSTS_DATE = new Intl.DateTimeFormat("ru", options).format(date);
 
 function renderPosts() {
-    const posts = getPosts();
+  const posts = getPosts();
 
-    let postsHTML = '';
+  let postsHTML = "";
 
-    posts.forEach(post => {
-        postsHTML += `
-            <div class='post'>
-                <div class='post__date'>${POSTS_DATE}</div>
-                <p class='post__title'>${post.title}</p>
-                <p class='post__text'>${post.text}</p>
-            </div>
-        `        
-    });
+  posts.forEach((post) => {
+    postsHTML += `
+        <div class='post'>
+            <div class='post__date'>${POSTS_DATE}</div>
+            <p class='post__title'>${post.title}</p>
+            <p class='post__text'>${post.text}</p>
+        </div>
+        `;
+  });
 
-   postsNode.innerHTML = postsHTML;
+  postsNode.innerHTML = postsHTML;
 }
-
