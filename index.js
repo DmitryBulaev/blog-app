ba2a276d593146ce8884b060303fbeb42a211443
Dmitry Bulaev <bulaevd74@mail.ru>
2024-08-26 14:32:45 +0500

    divided the blog into sections and added a block with posts

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ let postTitle = null;
 
 const titleInputNode = document.querySelector("[data-find = inputTitle]");
 const newPostButtonNode = document.getElementById("newPostButton");
+const postsNode = document.querySelector("[data-text = fullPosts]");
 
 newPostButtonNode.addEventListener("click", function () {
   postTitle = titleInputNode.value;
 
-  console.log(postTitle);
+  postsNode.innerText = postTitle;
 });
