fe7852c443d16776cf1db4d36ed9ebbd0364f804
Dmitry Bulaev <bulaevd74@mail.ru>
2024-08-26 13:24:48 +0500

    add js-logic for title and button

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(1);
+let postTitle = null;
+
+const titleInputNode = document.querySelector("[data-find = inputTitle]");
+const newPostButtonNode = document.getElementById("newPostButton");
+
+newPostButtonNode.addEventListener("click", function () {
+  postTitle = titleInputNode.value;
+
+  console.log(postTitle);
+});
